813f645c0ae2dfbed05a0c1a8c79376b41321f26
armandwipangestu <armandwi.pangestu7@gmail.com>
2023-06-14 02:36:30 +0700

    Stisla - Assets

---
+++ application/controllers/Stisla.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Stisla extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('stisla');
+    }
+}
