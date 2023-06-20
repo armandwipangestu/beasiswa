69d8e29a23306194c7678e562730d0239ff96767
Arman <armandwi.pangestu7@gmail.com>
2023-06-20 21:20:50 +0700

    Kampus - Kelas Program CRUD

--- application/controllers/Kampus.php
+++ application/controllers/Kampus.php
@@ -9,9 +9,9 @@ class Kampus extends CI_Controller
         cek_sudah_masuk();
     }
 
-    public function daftar_jurusan()
+    public function jurusan()
     {
-        $data['title'] = 'Daftar Jurusan';
+        $data['title'] = 'Jurusan';
         $data['user'] = $this->db->get_where('user_data', ['email' => $this->session->userdata('email')])->row_array();
         $data['jurusan'] = $this->db->get('kampus_jurusan')->result_array();
 
@@ -24,19 +24,19 @@ class Kampus extends CI_Controller
             $this->load->view('layout/header', $data);
             $this->load->view('layout/topbar');
             $this->load->view('layout/sidebar');
-            $this->load->view('kampus/daftar_jurusan');
+            $this->load->view('kampus/jurusan');
             $this->load->view('layout/footer');
         } else {
             $jurusan_name = $this->input->post('jurusan');
             $this->db->insert('kampus_jurusan', ['jurusan' => $jurusan_name]);
             $this->session->set_flashdata('message', '<div class="alert alert-success neu-brutalism mb-4">Jurusan <b>' . $jurusan_name . '</b> berhasil ditambahkan!</div>');
-            redirect('kampus/daftar_jurusan');
+            redirect('kampus/jurusan');
         }
     }
 
     public function ubah_jurusan()
     {
-        $data['title'] = 'Daftar Jurusan';
+        $data['title'] = 'Jurusan';
         $data['user'] = $this->db->get_where('user_data', ['email' => $this->session->userdata('email')])->row_array();
         $data['jurusan'] = $this->db->get('kampus_jurusan')->result_array();
 
@@ -49,7 +49,7 @@ class Kampus extends CI_Controller
             $this->load->view('layout/header', $data);
             $this->load->view('layout/topbar');
             $this->load->view('layout/sidebar');
-            $this->load->view('kampus/daftar_jurusan');
+            $this->load->view('kampus/jurusan');
             $this->load->view('layout/footer');
         } else {
             $id = $this->input->post('id');
@@ -59,14 +59,14 @@ class Kampus extends CI_Controller
             $this->db->where('id', $id);
             $this->db->update('kampus_jurusan', ['jurusan' => $jurusan]);
             $this->session->set_flashdata('message', '<div class="alert alert-warning neu-brutalism mb-4">Jurusan <b>' . $jurusanSebelum['jurusan'] . '</b> berhasil diubah menjadi <b>' . $jurusan . '</b>!</div>');
-            redirect('kampus/daftar_jurusan');
+            redirect('kampus/jurusan');
         }
     }
 
     public function get_jurusan($id)
     {
-        $menu = $this->db->query('SELECT * FROM kampus_jurusan WHERE id = ' . $id . '')->row();
-        exit(json_encode((array)$menu));
+        $jurusan = $this->db->query('SELECT * FROM kampus_jurusan WHERE id = ' . $id . '')->row();
+        exit(json_encode((array)$jurusan));
     }
 
     public function hapus_jurusan()
@@ -76,6 +76,76 @@ class Kampus extends CI_Controller
         $this->db->where('id', $jurusan_id);
         $this->db->delete('kampus_jurusan');
         $this->session->set_flashdata('message', '<div class="alert alert-danger neu-brutalism mb-4">Jurusan <b>' . $jurusan . '</b> berhasil dihapus!</div>');
-        redirect('kampus/daftar_jurusan');
+        redirect('kampus/jurusan');
+    }
+
+    public function kelas_program()
+    {
+        $data['title'] = 'Kelas Program';
+        $data['user'] = $this->db->get_where('user_data', ['email' => $this->session->userdata('email')])->row_array();
+        $data['kelas_program'] = $this->db->get('kampus_kelas_program')->result_array();
+
+        $this->form_validation->set_rules('kelas_program', 'Kelas Program', 'required|is_unique[kampus_kelas_program.kelas_program]', [
+            'required' => 'Nama Kelas Program tidak boleh kosong',
+            'is_unique' => 'Kelas Program ' . $this->input->post('kelas_program') .  ' sudah ada!'
+        ]);
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('layout/header', $data);
+            $this->load->view('layout/topbar');
+            $this->load->view('layout/sidebar');
+            $this->load->view('kampus/kelas_program');
+            $this->load->view('layout/footer');
+        } else {
+            $kelas_program = $this->input->post('kelas_program');
+            $this->db->insert('kampus_kelas_program', ['kelas_program' => $kelas_program]);
+            $this->session->set_flashdata('message', '<div class="alert alert-success neu-brutalism mb-4">Kelas Program <b>' . $kelas_program . '</b> berhasil ditambahkan!</div>');
+            redirect('kampus/kelas_program');
+        }
+    }
+
+    public function ubah_kelas_program()
+    {
+        $data['title'] = 'Kelas Program';
+        $data['user'] = $this->db->get_where('user_data', ['email' => $this->session->userdata('email')])->row_array();
+        $data['kelas_program'] = $this->db->get('kampus_kelas_program')->result_array();
+
+        $this->form_validation->set_rules('kelas_program', 'Kelas Program', 'required|is_unique[kampus_kelas_program.kelas_program]', [
+            'required' => 'Nama Kelas Program tidak boleh kosong',
+            'is_unique' => 'Kelas Program ' . $this->input->post('kelas_program') .  ' sudah ada!'
+        ]);
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('layout/header', $data);
+            $this->load->view('layout/topbar');
+            $this->load->view('layout/sidebar');
+            $this->load->view('kampus/kelas_program');
+            $this->load->view('layout/footer');
+        } else {
+            $id = $this->input->post('id');
+            $kelas_program = $this->input->post('kelas_program');
+            $kelasProgramSebelum = $this->db->get_where('kampus_kelas_program', ['id' => $id])->row_array();
+
+            $this->db->where('id', $id);
+            $this->db->update('kampus_kelas_program', ['kelas_program' => $kelas_program]);
+            $this->session->set_flashdata('message', '<div class="alert alert-warning neu-brutalism mb-4">Kelas Program <b>' . $kelasProgramSebelum['kelas_program'] . '</b> berhasil diubah menjadi <b>' . $kelas_program . '</b>!</div>');
+            redirect('kampus/kelas_program');
+        }
+    }
+
+    public function get_kelas_program($id)
+    {
+        $kelas_program = $this->db->query('SELECT * FROM kampus_kelas_program WHERE id = ' . $id . '')->row();
+        exit(json_encode((array)$kelas_program));
+    }
+
+    public function hapus_kelas_program()
+    {
+        $kelas_program_id = $this->uri->segment(3);
+        $kelas_program = $this->db->get_where('kampus_kelas_program', ['id' => $kelas_program_id])->row_array()['kelas_program'];
+        $this->db->where('id', $kelas_program_id);
+        $this->db->delete('kampus_kelas_program');
+        $this->session->set_flashdata('message', '<div class="alert alert-danger neu-brutalism mb-4">Jurusan <b>' . $kelas_program . '</b> berhasil dihapus!</div>');
+        redirect('kampus/kelas_program');
     }
 }
--- application/views/kampus/kelas_program.php
+++ application/views/kampus/kelas_program.php
@@ -6,28 +6,28 @@
         </div>
 
         <?= $this->session->flashdata('message') ?>
-        <?= form_error('jurusan', '<div class="alert alert-danger neu-brutalism mb-4">', '</div>') ?>
+        <?= form_error('kelas_program', '<div class="alert alert-danger neu-brutalism mb-4">', '</div>') ?>
 
-        <a href="#" data-toggle="modal" data-target="#modalJurusanBaru" class="btn btn-icon icon-left btn-primary mb-4 neu-brutalism"><i class="fas fa-plus"></i> Tambah Jurusan Baru</a>
+        <a href="#" data-toggle="modal" data-target="#modalKelasProgramBaru" class="btn btn-icon icon-left btn-primary mb-4 neu-brutalism"><i class="fas fa-plus"></i> Tambah Kelas Program Baru</a>
 
         <div class="table-responsive rounded">
             <table class="table table-hover table-bordered neu-brutalism-border display" id="myTable">
                 <thead>
                     <tr>
                         <th scope="col" class="text-dark">#</th>
-                        <th scope="col" class="text-dark">Jurusan</th>
+                        <th scope="col" class="text-dark">Kelas Program</th>
                         <th scope="col" class="text-dark">Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php $i = 1 ?>
-                    <?php foreach ($jurusan as $j) : ?>
+                    <?php foreach ($kelas_program as $kp) : ?>
                         <tr>
                             <th scope="row"><?= $i ?></th>
-                            <td><?= $j['jurusan']; ?></td>
+                            <td><?= $kp['kelas_program']; ?></td>
                             <td>
-                                <a href="#" onclick="ubah(<?= $j['id'] ?>)" class="btn btn-warning mr-2 neu-brutalism"><i class="fas fa-edit"></i> Ubah</a>
-                                <a class="btn btn-danger neu-brutalism hapus" data-id="<?= $j['id'] ?>" data-url="<?= base_url('kampus/hapus_jurusan') ?>" data-jurusan="<?= $j['jurusan'] ?>"><i class="fas fa-trash"></i> Hapus</a>
+                                <a href="#" onclick="ubah(<?= $kp['id'] ?>)" class="btn btn-warning mr-2 neu-brutalism"><i class="fas fa-edit"></i> Ubah</a>
+                                <a class="btn btn-danger neu-brutalism hapus" data-id="<?= $kp['id'] ?>" data-url="<?= base_url('kampus/hapus_kelas_program') ?>" data-kelas="<?= $kp['kelas_program'] ?>"><i class="fas fa-trash"></i> Hapus</a>
                             </td>
                         </tr>
                         <?php $i++ ?>
@@ -40,21 +40,21 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="modalJurusanBaru" tabindex="-1" role="dialog" aria-labelledby="modalJurusanBaruLabel" aria-hidden="true">
+<div class="modal fade" id="modalKelasProgramBaru" tabindex="-1" role="dialog" aria-labelledby="modalKelasProgramBaruLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content neu-brutalism-border">
             <div class="modal-header">
-                <h5 class="modal-title text-dark" id="newModalJurusanBaru">Tambah Jurusan Baru</h5>
+                <h5 class="modal-title text-dark" id="newModalKelasProgramBaru">Tambah Kelas Program Baru</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form action="<?= base_url('kampus/daftar_jurusan') ?>" method="POST">
+            <form action="<?= base_url('kampus/kelas_program') ?>" method="POST">
 
                 <div class="modal-body">
                     <div class="form-group">
-                        <label for="jurusan" class="form-label">Nama Jurusan</label>
-                        <input type="text" class="form-control" id="jurusan" name="jurusan">
+                        <label for="kelas_program" class="form-label">Nama Kelas Program</label>
+                        <input type="text" class="form-control" id="kelas_program" name="kelas_program">
                     </div>
                 </div>
 
@@ -68,22 +68,22 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="modalJurusanUbah" tabindex="-1" role="dialog" aria-labelledby="modalJurusanUbahLabel" aria-hidden="true">
+<div class="modal fade" id="modalKelasProgramUbah" tabindex="-1" role="dialog" aria-labelledby="modalKelasProgramUbahLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content neu-brutalism-border">
             <div class="modal-header">
-                <h5 class="modal-title modal-ubah text-dark" id="newModalJurusanUbah"></h5>
+                <h5 class="modal-title modal-ubah text-dark" id="newmodalKelasProgramUbah"></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form action="<?= base_url('kampus/ubah_jurusan') ?>" method="POST">
+            <form action="<?= base_url('kampus/ubah_kelas_program') ?>" method="POST">
                 <input type="hidden" class="form-control" id="idUbah" name="id">
 
                 <div class="modal-body">
                     <div class="form-group">
-                        <label for="jurusanUbah" class="form-label">Nama Jurusan</label>
-                        <input type="text" class="form-control" id="jurusanUbah" name="jurusan">
+                        <label for="kelasProgramUbah" class="form-label">Nama Kelas Program</label>
+                        <input type="text" class="form-control" id="kelasProgramUbah" name="kelas_program">
                     </div>
                 </div>
 
@@ -99,14 +99,14 @@
     const baseUrl = `<?= base_url() ?>`
 
     const ubah = (id) => {
-        $.get(`${baseUrl}kampus/get_jurusan/${id}`, (data) => {
-            const jurusan = $.parseJSON(data)
+        $.get(`${baseUrl}kampus/get_kelas_program/${id}`, (data) => {
+            const kelas_program = $.parseJSON(data)
             // console.log(jurusan)
 
-            $('.modal-ubah').text('Ubah Jurusan')
-            $('#idUbah').val(jurusan.id);
-            $('#jurusanUbah').val(jurusan.jurusan);
-            $('#modalJurusanUbah').modal('show');
+            $('.modal-ubah').text('Ubah Kelas Program')
+            $('#idUbah').val(kelas_program.id);
+            $('#kelasProgramUbah').val(kelas_program.kelas_program);
+            $('#modalKelasProgramUbah').modal('show');
         })
     }
 
@@ -115,10 +115,10 @@
         hm.addEventListener('click', () => {
             const dataId = hm.dataset.id
             const dataUrl = hm.dataset.url
-            const dataJurusan = hm.dataset.jurusan
+            const dataKelasProgram = hm.dataset.kelas
             Swal.fire({
                 icon: 'warning',
-                html: `Apakah anda yakin ingin menghapus <b>${dataJurusan}</b>?`,
+                html: `Apakah anda yakin ingin menghapus <b>${dataKelasProgram}</b>?`,
                 showCancelButton: true,
                 confirmButtonColor: '#d9534f',
                 cancelButtonColor: '#5cb85c',
